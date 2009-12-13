6fb36d1841434e3e0da443de670982050a9cfb58
Frank Terbeck <ft@bewatermyfriend.org>
2009-12-13 12:37:13 +0100

    Release version 0.3

--- taggit.h
+++ taggit.h
@@ -89,7 +89,7 @@ typedef int boolean;
  * taggit v0.2+git (debian 0.2-3)
  * \endcode
  */
-#define VERSION "0.2+git"
+#define VERSION "0.3"
 
 /**
  * Define a maximum set of tags in read and write maps
