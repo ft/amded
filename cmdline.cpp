5be3a58d228474eb37cb3bb6ccf8edddad1a3f01
Frank Terbeck <ft@bewatermyfriend.org>
2013-10-27 13:18:31 +0100

    Fix location of LICENCE in file headers
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- cmdline.cpp
+++ cmdline.cpp
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2013 taggit workers, All rights reserved.
- * Terms for redistribution and use can be found in doc/LICENCE.
+ * Terms for redistribution and use can be found in LICENCE.
  */
 
 /**
