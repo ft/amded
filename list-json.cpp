a682c292925b3dafed55da5249b537a0267c1f16
Frank Terbeck <ft@bewatermyfriend.org>
2015-05-17 21:12:18 +0200

    Actually add data to json output
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- list-json.cpp
+++ list-json.cpp
@@ -12,15 +12,50 @@
 #include <memory>
 #include <json/json.h>
 #include <json/writer.h>
+#include <stdlib.h>
 
+#include "list.h"
 #include "list-json.h"
 
 static Json::Value data;
 
-void
-amded_push_json(const struct amded_file &file __attribute__((unused)))
+static void
+push_item(std::string file, std::pair<const std::string, Value> &value)
 {
-    data["stuff"] = "is in here";
+    switch (value.second.get_type()) {
+    case TAG_INTEGER:
+        data[file][value.first] = value.second.get_int();
+        break;
+    case TAG_BOOLEAN:
+        data[file][value.first] = value.second.get_bool();
+        break;
+    case TAG_STRING:
+        data[file][value.first] = value.second.get_str().toCString(true);
+        break;
+    default:
+        std::cout << "Invalid type in: " << value.first << std::endl;
+        std::cout << "This is a bug. Please report!" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+}
+
+void
+amded_push_json(const struct amded_file &file)
+{
+    std::map<std::string, Value> basics;
+    std::map<std::string, Value> tags;
+    std::map<std::string, Value> props;
+
+    basics = amded_list_amded(file);
+    tags = amded_list_tags(file);
+    props = amded_list_audioprops(file.fh->audioProperties());
+
+    for (auto &iter : basics)
+        push_item(file.name, iter);
+    for (auto &iter : tags)
+        push_item(file.name, iter);
+    for (auto &iter : props)
+        push_item(file.name, iter);
 }
 
 void
