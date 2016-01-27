65722537e86bef3fc4bd380f79c3b281638796be
Frank Terbeck <ft@bewatermyfriend.org>
2016-01-27 01:26:31 +0100

    Update Copyright years in json code
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- list-json.cpp
+++ list-json.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015 amded workers, All rights reserved.
+ * Copyright (c) 2015-2016 amded workers, All rights reserved.
  * Terms for redistribution and use can be found in LICENCE.
  */
 
