abf46eb5b22e07fa6f23af4a3655cf49242be0f3
Frank Terbeck <ft@bewatermyfriend.org>
2018-07-01 16:32:25 +0200

    Update copyright years

--- file-spec.h
+++ file-spec.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2017 amded workers, All rights reserved.
+ * Copyright (c) 2013-2018 amded workers, All rights reserved.
  * Terms for redistribution and use can be found in LICENCE.
  */
 
--- info.h
+++ info.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009-2017 amded workers, All rights reserved.
+ * Copyright (c) 2009-2018 amded workers, All rights reserved.
  * Terms for redistribution and use can be found in LICENCE.
  */
 
